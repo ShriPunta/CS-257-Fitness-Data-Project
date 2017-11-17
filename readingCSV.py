d3b943b0415d67c775806f478a54cf369f709df9
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-11-17 14:00:15 -0800

    finding the time

--- readingCSV.py
+++ readingCSV.py
@@ -1,13 +1,16 @@
 import pandas as pd
-import os
 import glob
-base_dir = r'C:\Users\shrip\Pictures\url_downloads\test\\'   # use your path
+import time
+start_time = time.time()
+
+
+base_dir = r'C:\Users\shrip\Downloads\Geolife Trajectories 1.3\Data\\'   # use your path
 #all_files = glob.glob(os.path.join(base_dir, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
 
 all_filenames = []
 for filename in glob.glob(base_dir+'/**/*.plt', recursive=True):
     all_filenames.append(filename)
-
+print(len(all_filenames))
 #print(all_filenames)
 
 df_from_each_file = (pd.read_csv(f,usecols=[0,1],skiprows=6) for f in all_filenames)
@@ -30,7 +33,11 @@ for each_file in df_from_each_file:
     min_lat_from_file.append(each_file['Latitude'].min())
     min_long_from_file.append(each_file['Longitude'].min())'''
 
+print(max(max_lat_from_file))
 print(max(max_long_from_file))
+print(min(min_lat_from_file))
+print(min(min_long_from_file))
 
+print("--- %s seconds ---" % (time.time() - start_time))
 #concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
 # doesn't create a list, nor does it append to one
