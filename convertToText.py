605ea728a1b7c914c51783c217aefa38060bacf9
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-11-18 21:53:37 -0800

    optimizations

--- convertToText.py
+++ convertToText.py
@@ -1,26 +1,46 @@
 import os
 import random
+import time
+import glob
 
 base_dir = r'C:\Users\shrip\Pictures\url_downloads\test\PLTs\\'
 
+all_nfile_paths_list = []
+def fill_list_of_filenames(curr_filepath):
+    #for root, dirs, files in os.walk(curr_filepath, topdown=True):
+     # for filename in files:
+    for filename in glob.glob(curr_filepath + '/**/*.plt', recursive=True):
+        all_nfile_paths_list.append(filename)
+        #filepath = os.path.join(root,filename)
+        #print(filepath)
 
-def enter_dir(curr_filepath):
-    print(curr_filepath)
-    for root, dirs, files in os.walk(curr_filepath, topdown=True):
-      for filename in files:
-        filepath = os.path.join(root,filename)
-        print(filepath)
-        b,ext = os.path.splitext(filepath)
-        print(ext)
+        #print(filename.replace('\\','\\\\'))
+
+        #print(ext)
         #print(b)
         #del_start_lines(filepath)
-        rand_name = str(random.randrange(0,99999))
-        ind = filepath.find('.')
-        print(ind)
-        if ext == ".plt":
-            del_start_lines(filepath)
-            os.rename(filepath,filepath[0:ind]+rand_name+".csv")
-            print('-----------')
+
+
+
+def enter_dir(curr_filepath):
+    fill_list_of_filenames(curr_filepath)
+    #print('-------',all_nfile_paths_map)
+    for filename in all_nfile_paths_list:
+       # print(filename)
+       b, ext = os.path.splitext(filename)
+       if ext == ".plt":
+            rand_name = str(random.randrange(0, 99999))
+            #print('^&^&^&',filename)
+            new_file_path = filename[0:-4]+rand_name+".csv"
+            #print('$$$$$$',new_file_path)
+            #continue
+            try:
+                os.rename(filename,new_file_path)
+            except:
+                print('########')
+                continue
+            del_start_lines(new_file_path)
+            print('-')
 
     ''' elif ext == ".txt":
           os.rename(filepath, filepath[0:ind]+rand_name+ ".csv")
@@ -32,14 +52,16 @@ def defin_header():
 
 #delete first 6 lines and a header
 def del_start_lines(filename):
-    f = open(filename)
+    f = open(filename,'r+')
     lines = f.readlines()
     f.seek(0,0)
 
-    f = open(filename, 'w')
+    #f = open(filename, 'w')
     f.write(defin_header())
     f.write('\n')
     f.writelines(lines[6:])
     f.close()
 
+start_time = time.time()
 enter_dir(base_dir)
+print("--- %s seconds ---" % (time.time() - start_time))
--- readingCSV.py
+++ readingCSV.py
@@ -2,37 +2,50 @@ import pandas as pd
 import glob
 import time
 
-start_time = time.time()
-
-
-base_dir = r'C:\Users\shrip\Pictures\url_downloads\test\PLTs\\'   # use your path
-#all_files = glob.glob(os.path.join(base_dir, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
 
 all_filenames = []
-for filename in glob.glob(base_dir+'/**/*.csv', recursive=True):
-    all_filenames.append(filename)
-print(len(all_filenames))
-#print(all_filenames)
-i=1
-for each in all_filenames:
-   print(each)
-   try:
-      df = pd.read_csv(each,usecols=[0,1],skipinitialspace=True)
-      df.set_index('lat',inplace=True)
-      print(df)
-   except:
-      print('------')
-      continue
-   #print(df)
-   print(i)
-   i=i+1
-
-df_from_each_file = (pd.read_csv(f,usecols=[0,1],skiprows=6) for f in all_filenames)
-
 max_lat_from_file = []
 min_lat_from_file = []
 max_long_from_file = []
 min_long_from_file = []
+base_dir = r'C:\Users\shrip\Pictures\url_downloads\test\PLTs\\'   # use your path
+#all_files = glob.glob(os.path.join(base_dir, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
+
+
+def query_csv_filepaths(curr_dir):
+   #print('enterd')
+   for filename in glob.glob(curr_dir+'/**/*.csv', recursive=True):
+       all_filenames.append(filename)
+   print(len(all_filenames))
+
+def readallcsv_getmaxmin(filename_list):
+   i=1
+   for each in filename_list:
+      #print(each)
+      try:
+         df = pd.read_csv(each,usecols=[0,1],skipinitialspace=True)
+         max_lat, max_long = df.values.max(axis=0)
+         min_lat, min_long = df.values.min(axis=0)
+         max_lat_from_file.append(max_lat)
+         min_lat_from_file.append(min_lat)
+         max_long_from_file.append(max_long)
+         min_long_from_file.append(min_long)
+
+      except:
+         print('------')
+         continue
+      #print(df)
+      #print(i)
+      i=i+1
+
+   print(max(max_lat_from_file))
+   print(max(max_long_from_file))
+   print(min(min_lat_from_file))
+   print(min(min_long_from_file))
+
+
+'''df_from_each_file = (pd.read_csv(f,usecols=[0,1],skiprows=6) for f in all_filenames)
+
 
 
 for each_file in df_from_each_file:
@@ -42,16 +55,13 @@ for each_file in df_from_each_file:
    min_lat_from_file.append(min_lat)
    max_long_from_file.append(max_long)
    min_long_from_file.append(min_long)
-   ''' max_lat_from_file.append(each_file['Latitude'].max())
-    max_long_from_file.append(each_file['Longitude'].max())
-    min_lat_from_file.append(each_file['Latitude'].min())
-    min_long_from_file.append(each_file['Longitude'].min())'''
+   
+'''
 
-print(max(max_lat_from_file))
-print(max(max_long_from_file))
-print(min(min_lat_from_file))
-print(min(min_long_from_file))
-
+start_time = time.time()
+query_csv_filepaths(base_dir)
+if len(all_filenames) > 0:
+   readallcsv_getmaxmin(all_filenames)
 print("--- %s seconds ---" % (time.time() - start_time))
 #concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
 # doesn't create a list, nor does it append to one
